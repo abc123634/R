943a4ae2162cbaefb3784d80485083408ac8f1fe
LeeMeng <b98705001@gmail.com>
2015-09-11 08:06:21 +0800

    First commint

---
+++ R_Programming_Coursera/data.R
@@ -0,0 +1,6 @@
+data_frame1 <-
+structure(list(a = 1:10, b = 2:11), .Names = c("a", "b"), row.names = c(NA, 
+-10L), class = "data.frame")
+matrix1 <-
+structure(1:6, .Dim = 2:3, .Dimnames = list(c("a", "b"), c("d", 
+"e", "f")))
